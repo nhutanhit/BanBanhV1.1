d6709d12f43ee6a72bd88656f1f471b69675c294
SangNguyenBS <nguyenthienhoangsang@gmail.com>
2019-03-25 16:24:32 +0700

    hoàn thành Api/GetDataPreferenceController & Api/GetDetailOrderProductController

---
+++ Web_Manager/Controllers/Api/GetDataPreferenceController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Web_Manager.Api_FB_Manager.DAO_Manager;
+using Web_Manager.Api_FB_Manager.Models_Manager.Response;
+
+namespace Web_Manager.Controllers.Api
+{
+    public class GetDataPreferenceController : ApiController
+    {
+         DataAccess Db = new DataAccess();
+        static DataAccess stringConnect = new DataAccess();
+        static SqlConnection conn = new SqlConnection(stringConnect.ConnectionString());
+        static SqlCommand cmd;
+        static SqlDataAdapter adap;
+
+       
+       
+
+        /// <author>anh.tran</author>
+        /// <summary>
+        /// getDataProduct
+        /// </summary>
+        /// <returns></returns>
+        [System.Web.Http.HttpPost]
+        public ApiResponse GetDataProduct()
+        {
+            try
+            {
+                List<DataProductResponseModel> listproducts;
+                DataProductResponseModel pr;
+                listproducts = new List<DataProductResponseModel>();
+                conn.Close();
+                conn.Open();
+                cmd = new SqlCommand("select * from SanPham where trangthai = 1", conn);  // 1 còn bàn. 0 không bán nửa
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    pr = new DataProductResponseModel();
+                    pr.MaSanPham = Int32.Parse(reader["MaSanPham"].ToString());
+                    pr.TenSanPham = reader["TenSanPham"].ToString();
+                    pr.GiaBan = reader["GiaBan"].ToString();
+                    pr.GiaThiTruong = reader["GiaThiTruong"].ToString();
+                    pr.MoTa = reader["MoTa"].ToString();
+                    pr.SoLuongTon = Int32.Parse(reader["SoLuongTon"].ToString());
+                    pr.TrangThai = bool.Parse(reader["TrangThai"].ToString());
+                    pr.HinhAnh = reader["HinhAnh"].ToString();
+                    pr.NgayNhapHang = DateTime.Parse(reader["NgayNhapHang"].ToString());
+                    pr.MaThuongHieu = Int32.Parse(reader["MaThuongHieu"].ToString()); 
+                    listproducts.Add(pr);
+                }
+                conn.Close();
+                return ApiResponse.Success(listproducts);
+            }
+            catch (Exception)
+            {
+                return ApiResponse.Error();
+            }
+
+        }
+
+
+    }
+}
---
+++ Web_Manager/Controllers/Api/GetDetailOrderProductController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Web_Manager.Api_FB_Manager.DAO_Manager;
+using Web_Manager.Api_FB_Manager.Models_Manager.Request;
+using Web_Manager.Api_FB_Manager.Models_Manager.Response;
+
+namespace Web_Manager.Controllers.Api
+{
+    public class GetDetailOrderProductController : ApiController 
+    {
+        DataAccess db = new DataAccess();
+         Object DetailOrderList;
+
+        /// <author>anh.tran</author>
+        /// <summary>
+        /// GetOrderProduct
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ApiResponse GetOrderProduct([FromBody] GetDetailOrderProductRequestModel request )
+        {
+            try
+            {
+                try
+                {
+                    string stringSlq = "select DDH.MaDonHang,CTDDH.SoLuong, SP.TenSanPham, SP.GiaBan,Sp.HinhAnh, DDH.TinhTrangDonHang, AcUser.HoTenKH,DDH.NgayDat, DDH.DiaChiGiaoHang, DDH.SoDienThoai, AcUser.GioiTinh, AcUser.Email from  CHITIETDONDATHANG CTDDH, SANPHAM SP , DONDATHANG DDH, AccountUser AcUser where CTDDH.MaDonHang = '"+ request.Id + "' AND SP.MaSanPham = CTDDH.MaSanPham and CTDDH.MaDonHang = DDH.MaDonHang and DDH.MaKH = AcUser.MaKH";
+                    DetailOrderList = db.get_DaTaTable(stringSlq);
+                    if (DetailOrderList != null)
+                    {
+                        return ApiResponse.Success(DetailOrderList);
+                    }
+                    else
+                    {
+                        return ApiResponse.Success(0); // không tìm thấy dữ liệu  server
+                    }
+
+                }
+                catch (Exception)
+                {
+
+                    return ApiResponse.Error(-2);  // Không thể kết nối với severss
+                }
+
+            }
+            catch (Exception)
+            {
+                return ApiResponse.Error(-1);  // -1 Có lỗi trong quá trình xử lý. 
+            }
+        }
+    }
+}
